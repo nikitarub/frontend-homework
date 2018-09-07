882504fa0a6ed66d2a7f5eacdc5119407ee8db92
nikitarub <nikita.rub@yandex.ru>
2018-09-07 22:19:38 +0300

    Fixed code style and comment style

--- source/set.js
+++ source/set.js
@@ -5,20 +5,25 @@
   * Функция set 
   * 
   * Задаёт новое значение какого-либо параметра в объекте
-  * set (объект, параметр, новое значение){}
-  * проход по всем параметрам со спуском вглубь
+  * 
+  * @param {object} object - целевой объект
+  * @param {string} path - путь по которому произвести замену
+  * @param {*} value - значение для вставки 
   */
 function set(object, path, value) {
   const pathList = path.slice(1).split('.');
   const key = pathList.pop();
   const pointer = pathList.reduce(
-  	function(head, currentValue) 
+  	(head, currentValue) =>
   	{
-  		if (head[currentValue] === undefined) 
-  			head[currentValue] = {};
+      if (head[currentValue] === undefined) 
+      {
+        head[currentValue] = {};
+      }
   		return head[currentValue];
   	}, 
-  	object );
+    object 
+  );
 
   pointer[key] = value;
   return object;
